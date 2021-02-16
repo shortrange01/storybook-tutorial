410e0c9448787d19052c32e2d53461f2bcdc5a83
Hasegawa Taiji <shortrange01@gmail.com>
2021-02-17 00:58:26 +0900

    test: タスクリスト

---
+++ tests/unit/TaskList.spec.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import TaskList from '../../src/components/TaskList.vue';
+
+import { WithPinnedTasks } from '../../src/components/TaskList.stories';
+
+it('リストの先頭に固定（スター付き）されたタスクをレンダリングする', () => {
+    const Constructor = Vue.extend(TaskList);
+    const vm = new Constructor({
+        propsData: WithPinnedTasks.args,
+    }).$mount();
+    const firstTaskPinned = vm.$el.querySelector('.list-item:nth-child(1).TASK_PINNED');
+
+    expect(firstTaskPinned).not.toBe(null);
+})
